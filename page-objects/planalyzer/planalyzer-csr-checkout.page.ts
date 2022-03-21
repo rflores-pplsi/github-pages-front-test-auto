5f1caa9b27c4a8a1b5d18fa019ef01554baf4ee1
ls-ruchi <ruchigarg@legalshieldcorp.com>
2022-03-21 11:58:15 -0600

    mid changes

--- page-objects/planalyzer/planalyzer-csr-checkout.page.ts
+++ page-objects/planalyzer/planalyzer-csr-checkout.page.ts
@@ -5,11 +5,11 @@ import { OktaPage } from '../okta/okta.page';  // import the OktaPage for extens
 // ========================== Selectors ==================================
 let ddlEnvironment: string = 'text=EnvironmentUATSTGPROD >> select';
 let ddlChannel: string = 'text=ChannelChoose ChannelD2CNetworkSolutions >> select';
-let ddlSubChannel string = 'text=SubchannelChoose SubchannelLegalShieldIDShield >> select';
-let ddlRegion string = 'text=RegionChoose RegionAlabamaAlaskaArizonaArkansasCaliforniaColoradoConnecticutDela >> select';
-let ddlMarketLocal string = 'text=MarketLocaleChoose MarketLocaleen-CAen-USes-USfr-CA >> select';
-let txtPrepaidMonth string = '[placeholder="Pre-paid\ months"]';
-let txtCouponCode string = '[placeholder="Coupon\ code"]';
+let ddlSubChannel: string = 'text=SubchannelChoose SubchannelLegalShieldIDShield >> select';
+let ddlRegion: string = 'text=RegionChoose RegionAlabamaAlaskaArizonaArkansasCaliforniaColoradoConnecticutDela >> select';
+let ddlMarketLocal: string = 'text=MarketLocaleChoose MarketLocaleen-CAen-USes-USfr-CA >> select';
+let txtPrepaidMonth: string = '[placeholder="Pre-paid\ months"]';
+let txtCouponCode: string = '[placeholder="Coupon\ code"]';
 let btnShowReults: string = '';
 // let txtEmailOrUsername: string = '';
 
@@ -17,6 +17,18 @@ let btnShowReults: string = '';
 export class PlanalyzerCsrCheckoutPage extends OktaPage {
 
 // ========================== Process Methods ============================
+createLegalPlanOklahomaOrderRedirectToCheckout = async (): Promise<void> => { 
+    console.log(" - planalyzerCsrCheckoutPage.createLegalPlanOklahomaOrderRedirectToCheckout");
+    await this.select
+
+}
+
+selectEnvironment = async (): Promise<void> => { 
+    console.log(" - planalyzerCsrCheckoutPage.selectEnvironment");
+    await this.selectFromDropDownMenu();
+
+}
+
 
 // ========================== Navigate Methods ===========================
 
