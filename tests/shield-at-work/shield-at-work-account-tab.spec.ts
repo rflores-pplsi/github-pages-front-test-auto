59833dab6e967aeb79716a895e3509ebae930e41
Anastasiia <AnastasiiaAverianova@legalshieldcorp.com>
2022-04-07 17:36:58 -0400

    linting changes

--- tests/shield-at-work/shield-at-work-account-tab.spec.ts
+++ tests/shield-at-work/shield-at-work-account-tab.spec.ts
@@ -1,27 +1,23 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test } from "@playwright/test";
 
-import { ShieldAtWorkAccountTab } from '../../page-objects/shield-at-work/shield-at-work-account-tab.page';
+import { ShieldAtWorkAccountTab } from "../../page-objects/shield-at-work/shield-at-work-account-tab.page";
 
-
-
-
-let shieldAtWorkAccountTab:ShieldAtWorkAccountTab;
+let shieldAtWorkAccountTab: ShieldAtWorkAccountTab;
 
 test.beforeEach(async ({ page }) => {
-    shieldAtWorkAccountTab = new ShieldAtWorkAccountTab(page)
-    await shieldAtWorkAccountTab.navigateToShieldAtWorkAccountTab();
-
+  shieldAtWorkAccountTab = new ShieldAtWorkAccountTab(page);
+  await shieldAtWorkAccountTab.navigateToShieldAtWorkAccountTab();
 });
 
-test('Account information is displayed on the account tab', async ({ page }) => {
-    // Type in the search field 111452 group number and click on Search button
-    await shieldAtWorkAccountTab.navigateToShieldAtWorkAccountTab();
-    // Find group 111452 
-    await shieldAtWorkAccountTab.groupSearchByGroupNumber();
-    // Click on View Group button 
-    await shieldAtWorkAccountTab.clickViewGroup();
-    // Verify that account information is displayed
-    await shieldAtWorkAccountTab.assertAccountInformation();
-    
-
+test("Account information is displayed on the account tab", async ({
+  page,
+}) => {
+  // Type in the search field 111452 group number and click on Search button
+  await shieldAtWorkAccountTab.navigateToShieldAtWorkAccountTab();
+  // Find group 111452
+  await shieldAtWorkAccountTab.groupSearchByGroupNumber();
+  // Click on View Group button
+  await shieldAtWorkAccountTab.clickViewGroup();
+  // Verify that account information is displayed
+  await shieldAtWorkAccountTab.assertAccountInformation();
 });
--- tests/shield-at-work/shield-at-work-member-enrollment.spec.ts
+++ tests/shield-at-work/shield-at-work-member-enrollment.spec.ts
@@ -1,27 +1,24 @@
-import { test, expect, chromium } from '@playwright/test';
-import { ShieldAtWorkMemberEnrollment } from '../../page-objects/shield-at-work/shield-at-work-member-enrollment.page';
-
+import { test } from "@playwright/test";
+import { ShieldAtWorkMemberEnrollment } from "../../page-objects/shield-at-work/shield-at-work-member-enrollment.page";
 
 let shieldAtWorkMemberEnrollment: ShieldAtWorkMemberEnrollment;
 
 test.beforeEach(async ({ page }) => {
-    shieldAtWorkMemberEnrollment= new ShieldAtWorkMemberEnrollment(page)
-    await shieldAtWorkMemberEnrollment.navigateToShieldAtWork();
-
+  shieldAtWorkMemberEnrollment = new ShieldAtWorkMemberEnrollment(page);
+  await shieldAtWorkMemberEnrollment.navigateToShieldAtWork();
 });
 
-test('Edit button for contact information should be enabled', async ({ page }) => {
-    // Login with your credentials
-    await shieldAtWorkMemberEnrollment.loginWithCredentials();
-    // Click on Enroll new member button
-    await shieldAtWorkMemberEnrollment.clickEnrollNewMember();
-    //Fill out contact information 
-    await shieldAtWorkMemberEnrollment.fillOutContactInformation();
-    // Click on Continue button 
-    await shieldAtWorkMemberEnrollment.clickContinueButtonContactInfo();
-    //Verify that Edit button is enabled
-    await shieldAtWorkMemberEnrollment.assertEditButton();
-    
-    
-
+test("Edit button for contact information should be enabled", async ({
+  page,
+}) => {
+  // Login with your credentials
+  await shieldAtWorkMemberEnrollment.loginWithCredentials();
+  // Click on Enroll new member button
+  await shieldAtWorkMemberEnrollment.clickEnrollNewMember();
+  // Fill out contact information
+  await shieldAtWorkMemberEnrollment.fillOutContactInformation();
+  // Click on Continue button
+  await shieldAtWorkMemberEnrollment.clickContinueButtonContactInfo();
+  // Verify that Edit button is enabled
+  await shieldAtWorkMemberEnrollment.assertEditButton();
 });
