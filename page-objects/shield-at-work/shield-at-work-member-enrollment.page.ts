141b3205016b2c7a8d9ef7f830297410d1029ddb
Anastasiia <AnastasiiaAverianova@legalshieldcorp.com>
2022-04-07 16:35:57 -0400

    test commit

--- page-objects/shield-at-work/shield-at-work-member-enrollment.page.ts
+++ page-objects/shield-at-work/shield-at-work-member-enrollment.page.ts
@@ -17,7 +17,7 @@ let txtZipCode = '[placeholder="Zip code"]';
 let btnContinueContactInfo = '.lsux-content  form:nth-child(1) button > span';
 let btnEdit = '.lsux-content  .TZykF7nc8qs6i9k03RlmT > button > span';
 
-
+// comment
 
 
 
