35858eb38777fa530ee84aa467f8b63ffa32eb08
ErnestoAccel202110 <ernesto.segura@accelone.com>
2022-04-21 16:37:04 -0300

    Correct some comments

--- page-objects/channels/reports-commissions.page.ts
+++ page-objects/channels/reports-commissions.page.ts
@@ -1,20 +1,20 @@
-
 import { ChannelsHeaderPage } from '../../page-objects/channels/channels-header.page';
 import UrlsUtils from '../../utils/urls.utils';
-import { expect,Locator, Page, test } from '@playwright/test';
-import { matchers } from "expect-playwright"
+import { expect, Locator, Page, test } from '@playwright/test';
+import { matchers } from 'expect-playwright';
 // ========================== Selectors ==================================
 const txtBoxSearch: string = 'input[name="associateInputSearch"]';
-const organizationalBusinessReportBreadcrumb: string ='#root > div > div.lsux-content > div:nth-child(1) > div.lsux-crumbs > span';
-const organizationalBusinessReportTabDisplayed: string ='#root > div > div.lsux-content > div:nth-child(1) > div.lsux-tab--bar.lsux-tab--stretch > div.lsux-tab.active > div > h4';
-const txaAssociateNumber: string ='div.associate-info p';
-const txaSearchResult: string ='//*[@id="root"]/div/div[2]/div[1]/div[3]/div/div[1]/a/h2';
-const txaPersonal: string ='//*[@id="root"]/div/div[2]/div[1]/div[3]/div';
+const organizationalBusinessReportBreadcrumb: string = '#root > div > div.lsux-content > div:nth-child(1) > div.lsux-crumbs > span';
+const organizationalBusinessReportTabDisplayed: string =
+  '#root > div > div.lsux-content > div:nth-child(1) > div.lsux-tab--bar.lsux-tab--stretch > div.lsux-tab.active > div > h4';
+const txaAssociateNumber: string = 'div.associate-info p';
+const txaSearchResult: string = '//*[@id="root"]/div/div[2]/div[1]/div[3]/div/div[1]/a/h2';
+const txaPersonal: string = '//*[@id="root"]/div/div[2]/div[1]/div[3]/div';
 //Report -> Commissions
-const lblAssociateNumber: string ='.lsux-heading.lsux-heading--t28';
-const tabFastStartStatement: string ='//*[text()[contains(.,"Fast Start Statements")]]';
+const lblAssociateNumber: string = '.lsux-heading.lsux-heading--t28';
+const tabFastStartStatement: string = '//*[text()[contains(.,"Fast Start Statements")]]';
 // Associate Statement calendar
-const dtpSearchDates: string = "input[placeholder='Search dates']";   
+const dtpSearchDates: string = "input[placeholder='Search dates']";
 const dtpCalendar: string = "img[class*='lsux-icon--small']";
 const dtpYear: string = '.react-datepicker__year-select';
 const dtpMonth: string = '.react-datepicker__month-select';
@@ -23,7 +23,8 @@ const cboCountry: string = '.lsux-row div:nth-child(2) select';
 const countryUS: string = "div[class*='quarters'] > div:nth-of-type(2) > div > div > div > select>option";
 const countryCA: string = "#root div.tabs div.lsux-row.quarters.children4 > div:nth-child(2) select [ value='CAN']";
 const btnSearch: string = "button[class*='lsux-button--primary']";
-const lblAdvancedCommission: string = '#root > div > div.lsux-content > div.commissions-container > div.tabs > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div > div > h2';
+const lblAdvancedCommission: string =
+  '#root > div > div.lsux-content > div.commissions-container > div.tabs > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1) > div > div > h2';
 /**
  *
  *
@@ -33,65 +34,70 @@ const lblAdvancedCommission: string = '#root > div > div.lsux-content > div.comm
  */
 export class ReportsCommissionsPage extends ChannelsHeaderPage {
   // ========================== Process Methods ============================
-   /**
+  /**
    * pick month and year from date picker
    * @param {string} year
    * @param {string} month
    * @memberof ReportsCommissionsPage
    */
-    selectDateFromSearchDates = async (year: string, month: string): Promise<void> => {
-      console.log(' - ReportsCommissionsPage.selectDateFromSearchDates');
-      await this.selectFromDropDownMenu(dtpMonth, month);
-      await this.selectFromDropDownMenu(dtpYear, year);
-    };
-    selectCountry = async (country: string): Promise<void> => {
-      console.log(' - ReportsCommissionsPage.selectCountry');
-      await this.selectFromDropDownMenu(cboCountry, country);
-    };
+  selectDateFromSearchDates = async (year: string, month: string): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.selectDateFromSearchDates');
+    await this.selectFromDropDownMenu(dtpMonth, month);
+    await this.selectFromDropDownMenu(dtpYear, year);
+  };
+  /**
+   * select a country from a combo
+   * @param {string} country
+   * @memberof ReportsCommissionsPage
+   */
+  selectCountry = async (country: string): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.selectCountry');
+    await this.selectFromDropDownMenu(cboCountry, country);
+  };
   // ========================== Navigate Methods ===========================
   navigateToReportsBusinessOrganizationalPage = async (): Promise<void> => {
-      console.log(' - ReportsCommissionsPage.navigateToReportsBusinessOrganizationalPage');
-      // Navigate to Business Solution  Page
-      await this.navigateToPage(UrlsUtils.channelsUrls.reportsbusinessorganizational.url);
+    console.log(' - ReportsCommissionsPage.navigateToReportsBusinessOrganizationalPage');
+    // Navigate to Business Organizational Page
+    await this.navigateToPage(UrlsUtils.channelsUrls.reportsbusinessorganizational.url);
   };
   navigateToReportsCommissionsPage = async (): Promise<void> => {
     console.log(' - ReportsCommissionsPage.navigateToReportsCommissionsPage');
-    // Navigate to Business Solution  Page
+    // Navigate to Business Report Commission Page
     await this.navigateToPage(UrlsUtils.channelsUrls.reportscommissions.url);
   };
   // ========================== Click Methods ==============================
   clickTxtBoxSearch = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on text box for search
     console.log(' - ReportsCommissionsPage.clicktxtBoxSearch');
     await this.page.click(txtBoxSearch);
   };
   clickSearchResult = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on a search result
     console.log(' - ReportsCommissionsPage.clickSearchResult');
     await this.page.click(txaSearchResult);
   };
   clickOnSearchDates = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on data picker
     console.log(' - ReportsCommissionsPage.clickOnSearchDates');
     await this.page.click(dtpSearchDates);
   };
   clickOnCalendarDate = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click  on Calendar
     console.log(' - ReportsCommissionsPage.clickOnCalendarDate');
     await this.page.click(dtpCalendar);
   };
   clickOnSearchDatesYear = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on year combo
     console.log(' - ReportsCommissionsPage.clickOnSearchDatesYear');
     await this.page.click(dtpYear);
   };
   clickOnSearchDatesMonth = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on month combo
     console.log(' - ReportsCommissionsPage.clickOnSearchDatesMonth');
     await this.page.click(dtpMonth);
   };
   clickOnSearchDatesDay5 = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on day 5 from calendar
     console.log(' - ReportsCommissionsPage.clickOnSearchDatesDay');
     await this.assertElementIsEnabled(dtpDay5th);
     const locator = this.page.locator("(//div[contains(@class,'react-datepicker__day--005')])[1]");
@@ -99,7 +105,7 @@ export class ReportsCommissionsPage extends ChannelsHeaderPage {
     await this.page.click(dtpDay5th);
   };
   clickOnSearchButton = async (): Promise<void> => {
-    // Click Terms of Service link
+    // Click on Search button
     console.log(' - ReportsCommissionsPage.clickOnSearchButton');
     await this.page.click(btnSearch);
   };
@@ -169,5 +175,3 @@ export class ReportsCommissionsPage extends ChannelsHeaderPage {
     await this.assertElementIsVisible(lblAdvancedCommission);
   };
 }
-
-     
