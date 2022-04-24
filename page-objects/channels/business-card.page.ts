80857a2cc3ac4b4f6720f9a384bfc33dc4d7d9e2
ErnestoAccel202110 <ernesto.segura@accelone.com>
2022-04-24 13:30:29 -0300

    Solve PR comments

--- page-objects/channels/business-card.page.ts
+++ page-objects/channels/business-card.page.ts
@@ -1,9 +1,8 @@
-
 import { ChannelsHeaderPage } from '../../page-objects/channels/channels-header.page';
 import UrlsUtils from '../../utils/urls.utils';
-import { expect,Locator, Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 // ========================== Selectors ==================================
-const titleMarketingWebsitePreferences: string = 'h1.lsux-heading.lsux-heading--t28';
+const lblTitleMarketingWebsitePreferences: string = 'h1.lsux-heading.lsux-heading--t28';
 const txtFirstName: string = '[name="firstName"]';
 const txtLastName: string = '[name="lastName"]';
 const txtPhoneNumber: string = '[name="cellPhone"]';
@@ -16,47 +15,44 @@ const chkDisplayOnWebsite: string = 'label.lsux-cb-container';
  * @extends {ChannelsHeaderPage}
  */
 export class BusinessCard extends ChannelsHeaderPage {
-    // ========================== Process Methods ============================
-    // ========================== Navigate Methods ===========================
-    navigateToBusinessCardPage = async (): Promise<void> => {
-        console.log(' - BusinessCard.navigateToBusinessCardPage');
-        // Navigate to Business Solution  Page
-        await this.navigateToPage(UrlsUtils.channelsUrls.businesscard.url);
-        
-      };
-    // ========================== Click Methods ==============================
-    // ========================== Assertion Methods ==========================
-    assertPageShow = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertPageShow');
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      await this.assertElementIsVisible(titleMarketingWebsitePreferences);
-    };
-    assertPageTitle = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertPageTitle');
-      const strTitle = 'Organizational Business Report';
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      //expect.stringMatching( strTitle.match(this.page.title.toString()) );
-    };
-    assertFirstNameIsDisplayed = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertBreadcrumbLinkIsDisplayed');
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      await this.assertElementIsVisible(txtFirstName);
-    };
-    assertLastNameIsDisplayed = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      await this.assertElementIsVisible(txtLastName);
-    };
-    assertPhoneNumberIsDisplayed = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      await this.assertElementIsVisible(txtPhoneNumber);
-    };
-    assertDisplayOnWebsite = async (): Promise<void> => {
-      console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
-      await this.page.waitForSelector(titleMarketingWebsitePreferences);
-      await this.assertElementIsVisible(chkDisplayOnWebsite);
-    };
-    
+  // ========================== Process Methods ============================
+  // ========================== Navigate Methods ===========================
+  navigateToBusinessCardPage = async (): Promise<void> => {
+    console.log(' - BusinessCard.navigateToBusinessCardPage');
+    // Navigate to Business Solution  Page
+    await this.navigateToPage(UrlsUtils.channelsUrls.businesscard.url);
+  };
+  // ========================== Click Methods ==============================
+  // ========================== Assertion Methods ==========================
+  assertPageShow = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertPageShow');
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementIsVisible(lblTitleMarketingWebsitePreferences);
+  };
+  assertPageTitle = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertPageTitle');
+    const strTitle = 'Marketing Website Preferences';
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementHasText(lblTitleMarketingWebsitePreferences, strTitle);
+  };
+  assertFirstNameIsDisplayed = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertBreadcrumbLinkIsDisplayed');
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementIsVisible(txtFirstName);
+  };
+  assertLastNameIsDisplayed = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementIsVisible(txtLastName);
+  };
+  assertPhoneNumberIsDisplayed = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementIsVisible(txtPhoneNumber);
+  };
+  assertDisplayOnWebsite = async (): Promise<void> => {
+    console.log(' - BusinessCard.assertOrganizationalBusinessReportTaIsDisplayed');
+    await this.page.waitForSelector(lblTitleMarketingWebsitePreferences);
+    await this.assertElementIsVisible(chkDisplayOnWebsite);
+  };
 }
-     
--- page-objects/channels/channels-header.page.ts
+++ page-objects/channels/channels-header.page.ts
@@ -11,11 +11,10 @@ const txtPassword: string = '[placeholder="Password"]';
  *
  * @export
  * @class ChannelsHeaderPage
- * @extends 
+ * @extends
  */
 export class ChannelsHeaderPage extends LoginPage {
   loginPage = new LoginPage(this.page);
-  
 
   // ========================== Process Methods ============================
   // ========================== Navigate Methods ===========================
@@ -23,13 +22,10 @@ export class ChannelsHeaderPage extends LoginPage {
     console.log(' - ChannelsHeaderPage.navigateToPage');
     // Navigate to Account Plans Page
     await this.goTo(url);
-    // login with Okta
     await this.loginPage.loginWithEnterKey(associateAdvantagePlus.username, associateAdvantagePlus.password);
     // Wait for page to finish loading
     await this.page.waitForLoadState('networkidle');
-    
   };
   // ========================== Click Methods ==============================
   // ========================== Assertion Methods ==========================
 }
-
