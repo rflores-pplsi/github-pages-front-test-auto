67e14c21e001207448a1786438ae5665a8ad7c83
abdelmchaar <97123221+abdelmchaar@users.noreply.github.com>
2022-08-18 10:54:30 -0400

    Resolved conflicts

---
+++ src/page-objects/associate-office/associate-office-perks.page.ts
@@ -0,0 +1,132 @@
+import { expect } from '@playwright/test';
+import UrlsUtils from '../../utils/urls.utils';
+import { BasePage } from '../base.page';
+
+// ========================== Selectors ==========================
+const banner: string = '#banner-perks';
+const bannerTitle: string = '#banner-perks h2';
+const bannerDescription: string = '#banner-perks span';
+const textPerks: string = '#text-perks p';
+const featuredPerkTitle: string = '//*[text()="Featured Offer"]';
+const featuredPerkDescriptionP1: string = '//*[@class="information"]//p[1]';
+const featuredPerkDescriptionP2: string = '//*[@class="information"]//p[2]';
+const featuredPerkDescriptionP3: string = '//*[@class="information"]//p[3]';
+const featuredPerkImage: string = '#featured-perks .lsux-col.image';
+const associatedPerks: string = '//p[text()="ASSOCIATEPerks"]';
+const availCanText: string = '#perks .lsux-col.canada span';
+const availCanImg: string = '#perks .lsux-col.canada img';
+const perkDisclaimer: string = '.perks-disclaimer';
+
+  //John Addison Leadership perk
+const JALeadershipPerk: string = "div[class*='thirds'] > div:nth-of-type(1) > div > div > p";
+const JALeadershipPerkImg: string = "img[src*='2ef8a10b28e9943d741b6ceccf14c336.png']";
+const JALeadershipPerkDescription: string = '.card-perks > div > div > div:nth-child(2)  h1';
+const JALeadershipPerkComment: string = '.card-perks div:nth-child(2)  div > div > div > p';
+const JALeadershipPerkShopNow: string = "a[href*='ls-perks/']";
+
+/**
+ * @export
+ * @class PerksPage
+ * @extends {BasePage}
+ */
+export class PerksPage extends BasePage {
+  // ========================== Process Methods ==========================
+
+perk = async(perkTitle: string): Promise<void> => {
+  await this.page.waitForSelector(`//*[text()="${perkTitle}"]`);
+};
+
+description = async(perkTitle:string): Promise<void> => {
+  const perk = await this.page.waitForSelector(`//*[text()="${perkTitle}"]`);
+}
+
+  // ========================== Navigate Methods ==========================
+
+
+  // ========================== Click Methods ==========================
+
+
+
+  // ========================== Assertion Methods ==========================
+
+  assertPerksPageUrl = async (): Promise<void> => {
+  console.log(' - perksPage.assertPerksPageUrl');
+  // Confirm Perks Page URL is reached
+  await expect(this.page).toHaveURL(UrlsUtils.channelsUrls.perks.url +'?login_redirect=1');
+  // Wait for document to load before subsequent steps
+  await this.page.waitForLoadState('domcontentloaded');
+  };
+
+  assertBannerIsDisplayed = async(): Promise<void> => {
+  console.log(' - perksPage.assertBannerIsDisplayed');
+  await this.page.waitForSelector(banner);
+  await this.assertElementIsVisible(banner);
+  };
+
+  assertBannerTitleText = async (): Promise<void> => {
+  console.log(' - perksPage.assertBannerTitleText');
+  await this.page.waitForSelector(bannerTitle);
+  const strTitle = 'ASSOCIATEPerks';
+  await this.assertElementHasText(bannerTitle, strTitle);
+  };
+
+  assertBannerDescriptionText = async (): Promise<void> => {
+  console.log(' - perksPage.assertBannerDescriptionText');
+  await this.page.waitForSelector(bannerDescription);
+  const strDescription = 'Exclusive Discounts to take your business to the next level';
+  await this.assertElementHasText(bannerDescription, strDescription);
+  };
+
+  assertPerkText = async (): Promise<void> => {
+  console.log(' - perksPage.assertPerkText');
+  await this.page.waitForSelector(textPerks);
+  const strTitle = 'From office supplies to health care, PPLSI has partnered with these best in class services to help you manage and grow your business.';
+  await this.assertElementHasText(textPerks, strTitle);
+  };
+
+  assertFeaturedOfferTitleText = async (): Promise<void> => {
+  console.log(' - perksPage.assertFeaturedOfferTitleText');
+  await this.page.waitForSelector(featuredPerkTitle);
+  await this.assertElementIsVisible(featuredPerkTitle);
+  };
+
+  assertFeaturedPerkDescriptionText = async (): Promise<void> => {
+  console.log(' - perksPage.assertFeaturedPerkDescriptionText');
+  await this.page.waitForSelector(bannerDescription);
+  const strTitle1 = 'John Addison Leadership';
+  await this.assertElementHasText(featuredPerkDescriptionP1, strTitle1);
+  const strTitle2 = 'Use code 3THSPCVF for 10% off during checkout. (This is a one-time use code.)';
+  await this.assertElementHasText(featuredPerkDescriptionP2, strTitle2);
+  const strTitle3 = 'If you’re a unifier and you love to help others. If you understand that when we help others get what they want then we get what we want, I’d love for you to learn more about #MissionLeadership. I think we can do great things together.';
+  await this.assertElementHasText(featuredPerkDescriptionP3, strTitle3);
+  };
+
+  assertFeaturedPerkImageDisplayed = async(): Promise<void> => {
+  console.log(' - perksPage.assertFeaturedPerkImageDisplayed');
+  await this.page.waitForSelector(featuredPerkImage);
+  await this.assertElementIsVisible(featuredPerkImage);
+  };
+
+  assertASSOCIATEPerksDisplayed = async(): Promise<void> => {
+  console.log(' - perksPage.assertASSOCIATEPerksDisplayed');
+  await this.page.waitForSelector(associatedPerks);
+  await this.assertElementIsVisible(associatedPerks);
+  };
+
+  assertAvailableCanDisplayed = async(): Promise<void> => {
+  console.log(' - perksPage.assertAvailableCanDisplayed');
+  await this.page.waitForSelector(availCanText);
+  await this.assertElementIsVisible(availCanText);
+  };
+
+  assertAvailableCanImageDisplayed = async(): Promise<void> => {
+  console.log(' - perksPage.assertAvailableCanImageDisplayed');
+  await this.page.waitForSelector(availCanImg);
+  await this.assertElementIsVisible(availCanImg);
+  };
+
+  assertPerkDescriptionDisplayed = async(perkTitle:string): Promise<void> => {
+  console.log(' - perksPage.assertPerkImageDisplayed');
+  await this.description(perkTitle);
+  };
+}
---
+++ src/page-objects/associate-office/associate-office-reports-commissions.page.ts
@@ -0,0 +1,249 @@
+import { LoginPage } from '../login/login.page';
+import { expect } from '@playwright/test';
+import { revenueReports } from '../../utils/user.utils';
+import { associateReportsCommissions } from '../../utils/user.utils';
+import UrlsUtils from '../../utils/urls.utils';
+
+// ========================== Selectors ==================================
+
+const lblAssociateNumber: string = '.lsux-heading.lsux-heading--t28';
+const dtpSearchDates: string = "input[placeholder='Search dates']";
+const dtpCalendar: string = "img[class*='lsux-icon--small']";
+const dtpYear: string = '.react-datepicker__year-select';
+const dtpMonth: string = '.react-datepicker__month-select';
+const dtpDay2: string = "(//div[contains(@class,'react-datepicker__day--002')])[1]";
+const dtpDay5: string = "(//div[contains(@class,'react-datepicker__day--005')])[1]";
+const cboCountry: string = '.lsux-row div:nth-child(2) select';
+const countryUS: string = "div[class*='quarters'] > div:nth-of-type(2) > div > div > div > select>option";
+const countryCA: string = "#root div.tabs div.lsux-row.quarters.children4 > div:nth-child(2) select [ value='CAN']";
+const btnSearch: string = 'button[class*="lsux-button--primary"]';
+const lblAssociateLevel: string = "div[class='lsux-row plain children3']";
+const lblAssociateFullName: string = "(//span[contains(@class,'lsux-text')])[1]";
+const lblAssociateStreet: string = "(//span[contains(@class,'lsux-text')])[2]";
+const lblAssociateCityStateZipCode: string = "(//span[contains(@class,'lsux-text')])[3]";
+const tabAssociateStatements: string = "//h4[.='Associate Statements']";
+const tabPendingStatements: string = "//h4[.='Pending Statements']";
+const tabFastStartStatements: string = "//h4[.='Fast Start Statements']";
+const tabRevenueReport: string = "//h4[.='Revenue Report Statements']";
+const lblSearchCommissionStatements: string = "//span[.='Search Commission Statements']";
+const dtpSelectCalculation: string = "div[class*='quarters'] > div:nth-of-type(3) > div > div > div > select";
+const lblAdvancedCommission: string = '.tabs > div > div:nth-child(2) > div:nth-child(1) h2';
+const lblEarnedCommission: string = '.lsux-content div:nth-child(2) div:nth-child(2) h2';
+const lblComAdvanceBalance: string = "#root div:nth-child(3) > div > div:nth-child(1) h2'";
+const lblComByOrgCalculation: string = '#root div > div:nth-child(4) h2';
+const lblCommissionAdjustments: string = '#root div > div:nth-child(5) h2';
+const lblPersonalMemberships: string = '#root div > div:nth-child(6) h2';
+const lblPersonalCancellations: string = '#root div > div:nth-child(7) h2';
+const singleReport: string = 'div:nth-child(1) h2';
+/**
+ *
+ * @export
+ * @class ChannelsHeaderPage
+ * @extends
+ */
+export class ReportsCommissionsPage extends LoginPage {
+  // ========================== Process Methods ============================
+  /**
+   * pick month and year from date picker
+   * @param {string} year
+   * @param {string} month
+   * @memberof ReportsCommissionsPage
+   */
+  selectDateFromSearchDates = async (year: string, month: string): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.selectDateFromSearchDates');
+    await this.selectFromDropDownMenu(dtpMonth, month);
+    await this.selectFromDropDownMenu(dtpYear, year);
+  };
+  /**
+   * select a country from a combo
+   * @param {string} country
+   * @memberof ReportsCommissionsPage
+   */
+  selectCountry = async (country: string): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.selectCountry');
+    await this.selectFromDropDownMenu(cboCountry, country);
+  };
+  // ========================== Navigate Methods ===========================
+  navigateToReportsCommissionsPage = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.navigateToReportsCommissionsPage');
+    // Navigate to Report Commission Page
+    await this.goTo(UrlsUtils.channelsUrls.reportscommissions.url);
+    await this.login(revenueReports.username, revenueReports.password);
+    await this.page.waitForSelector(tabRevenueReport);
+  };
+
+  navigateToReportsCommissionsPage2 = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.navigateToReportsCommissionsPage');
+    // Navigate to Report Commission Page
+    await this.goTo(UrlsUtils.channelsUrls.reportscommissions.url);
+    await this.login(associateReportsCommissions.username, associateReportsCommissions.password);
+    await this.page.waitForSelector(lblAssociateNumber);
+  };
+  // ========================== Click Methods ==============================
+  clickOnSearchDates = async (): Promise<void> => {
+    // Click on data picker
+    console.log(' - ReportsCommissionsPage.clickOnSearchDates');
+    await this.page.click(dtpSearchDates);
+  };
+
+  clickOnCalendarDate = async (): Promise<void> => {
+    // Click  on Calendar
+    console.log(' - ReportsCommissionsPage.clickOnCalendarDate');
+    await this.page.click(dtpCalendar);
+  };
+
+  clickOnSearchDatesYear = async (): Promise<void> => {
+    // Click on year combo
+    console.log(' - ReportsCommissionsPage.clickOnSearchDatesYear');
+    await this.page.click(dtpYear);
+  };
+
+  clickOnSearchDatesMonth = async (): Promise<void> => {
+    // Click on month combo
+    console.log(' - ReportsCommissionsPage.clickOnSearchDatesMonth');
+    await this.page.click(dtpMonth);
+  };
+
+  clickOnSearchDatesDay2 = async (): Promise<void> => {
+    // Click on day 2 from calendar
+    console.log(' - ReportsCommissionsPage.clickOnSearchDatesDay');
+    await this.assertElementIsEnabled(dtpDay2);
+    const locator = this.page.locator(dtpDay2);
+    await expect(locator).toBeEnabled();
+    await this.page.click(dtpDay2);
+  };
+
+  clickOnSearchDatesDay5 = async (): Promise<void> => {
+    // Click on day 2 from calendar
+    console.log(' - ReportsCommissionsPage.clickOnSearchDatesDay');
+    await this.assertElementIsEnabled(dtpDay5);
+    const locator = this.page.locator(dtpDay5);
+    await expect(locator).toBeEnabled();
+    await this.page.click(dtpDay5);
+  };
+
+  clickOnSearchButton = async (): Promise<void> => {
+    // Click on Search button
+    console.log(' - ReportsCommissionsPage.clickOnSearchButton');
+    await this.page.click(btnSearch);
+  };
+
+  clickOnPendingStatements = async (): Promise<void> => {
+    // Click on Pending Statements tab
+    console.log(' - ReportsCommissionsPage.clickOnPendingStatements');
+    await this.page.click(tabPendingStatements);
+  };
+
+  clickOnFastStartStatements = async (): Promise<void> => {
+    // Click on Fast Start Statements tab
+    console.log(' - ReportsCommissionsPage.clickOnFastStartStatements');
+    await this.page.click(tabFastStartStatements);
+  };
+
+  clickOnRevenueReportTab = async (): Promise<void> => {
+    // Click on Revenue Report tab
+    console.log(' - ReportsCommissionsPage.clickOnRevenueReportTab');
+    await this.page.click(tabRevenueReport);
+  };
+
+  // ========================== Assertion Methods ==========================
+  assertReportsCommissionsPageShow = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertReportsCommissionsPageShow');
+    await this.assertElementIsVisible(lblAssociateNumber);
+    await this.assertElementIsVisible(lblAssociateLevel);
+  };
+
+  assertAssociateAddressInfo = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertAssociateAddressInfo');
+    await this.assertElementIsVisible(lblAssociateFullName);
+    await this.assertElementIsVisible(lblAssociateStreet);
+    await this.assertElementIsVisible(lblAssociateCityStateZipCode);
+  };
+
+  assertTabAssociateStatementsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertTabAssociateStatementsIsDisplayed');
+    await this.assertElementIsVisible(tabAssociateStatements);
+  };
+
+  assertTabPendingStatementsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertTabPendingStatementsIsDisplayed');
+    await this.assertElementIsVisible(tabPendingStatements);
+  };
+
+  assertTabFastStartStatementsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertTabRevenueReportsIsDisplayed');
+    await this.assertElementIsVisible(tabFastStartStatements);
+  };
+
+  assertTabRevenueReportIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertTabRevenueReportsIsDisplayed');
+    await this.page.waitForSelector(tabRevenueReport);
+    await this.assertElementIsVisible(tabRevenueReport);
+  };
+
+  assertSearchDatesIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertSearchDatesIsDisplayed');
+    await this.assertElementIsVisible(dtpSearchDates);
+  };
+
+  assertComboCountryIsEnable = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertComboCountryIsEnable');
+    await this.assertElementIsEnabled(cboCountry);
+  };
+
+  assertComboCountryIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertComboCountryIsDisplayed');
+    await this.assertElementIsVisible(cboCountry);
+  };
+
+  assertSelectCalculationIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertSelectCalculationIsDisplayed');
+    await this.assertElementIsVisible(dtpSelectCalculation);
+  };
+
+  assertLblSearchCommissionStatementsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertLblSearchCommissionStatementsIsDisplayed');
+    await this.assertElementIsVisible(lblSearchCommissionStatements);
+  };
+
+  assertBtnSearchIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertBtnSearchIsDisplayed');
+    await this.assertElementIsVisible(btnSearch);
+  };
+
+  assertAdvancedCommissionIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertAdvancedCommissionIsDisplayed');
+    await this.assertElementIsVisible(lblAdvancedCommission);
+  };
+
+  assertEarnedCommissionIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertEarnedCommissionIsDisplayed');
+    await this.assertElementIsVisible(lblEarnedCommission);
+  };
+
+  assertCommissionAdvanceBalanceIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertCommissionAdvanceBalanceIsDisplayed');
+    await this.page.waitForSelector(lblComAdvanceBalance);
+    await this.assertElementIsVisible(lblComAdvanceBalance);
+  };
+
+  assertComByOrganizationCalculationIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertComByOrganizationCalculationIsDisplayed');
+    await this.assertElementIsVisible(lblComByOrgCalculation);
+  };
+
+  assertCommissionAdjustmentsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertCommissionAdjustmentsIsDisplayed');
+    await this.assertElementIsVisible(lblCommissionAdjustments);
+  };
+
+  assertPersonalMembershipsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertPersonalMembershipsIsDisplayed');
+    await this.assertElementIsVisible(lblPersonalMemberships);
+  };
+
+  assertPersonalCancellationsIsDisplayed = async (): Promise<void> => {
+    console.log(' - ReportsCommissionsPage.assertPersonalCancellationsIsDisplayed');
+    await this.assertElementIsVisible(lblPersonalCancellations);
+  };
+}
