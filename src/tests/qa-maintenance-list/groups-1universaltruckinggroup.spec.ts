c5801b5ad046e79b268930601c859dd68bad5dc4
aidapira <aidapirahmadi@pplsi.com>
2022-11-08 14:47:20 -0800

    Updated groups-1universaltruckinggroup.spec.ts

--- src/tests/qa-maintenance-list/groups-1universaltruckinggroup.spec.ts
+++ src/tests/qa-maintenance-list/groups-1universaltruckinggroup.spec.ts
@@ -1,35 +1,35 @@
 /* eslint-disable no-undef */
-import { test } from '@playwright/test';
-import { UniversalTruckingPage } from '../..//page-objects/qa-maintenance-list/1UniversalTruckinggroup.page';
-// create instance of Page
-let universalTruckingPage: UniversalTruckingPage;
+// import { test } from '@playwright/test';
+// import { UniversalTruckingPage } from '../..//page-objects/qa-maintenance-list/1UniversalTruckinggroup.page';
+// // create instance of Page
+// let universalTruckingPage: UniversalTruckingPage;
 
 // Setup environment before each test
-test.beforeEach(async ({ page }) => {
-  universalTruckingPage = new UniversalTruckingPage(page);
-  // test.slow triples the default wait times
-  test.slow();
-  // await checkoutConfirmationPage.navigateToCheckoutConfirmationPage('Alaska');
-});
-test('Universal Trucking Group', async ({ page }) => {
-  test.slow;
-  // Go to https://www.shieldbenefits.com/bestmoneymoves/overview
-  await universalTruckingPage.navigateTo1UniversalTruckingGroupPage();
-  // Confirm button takes user to W3 URL
-  await universalTruckingPage.assertW3Url();
-  // Click on SignUp tab
-  await universalTruckingPage.clickTabSigningUp();
-  // Select a language
-  await universalTruckingPage.selectlanguage('French');
-  // Select a state
-  await universalTruckingPage.selectStateUniversalTruckingPage('Texas');
-  await universalTruckingPage.clickBtnESelect();
-  // Select a plan
-  await universalTruckingPage.clickBtnSelectPlan('Sélectionnez votre plan juridique');
-  // Verify the Plan name
-  await universalTruckingPage.assertSelectedPlanAndParMoiTxt();
-  // Verify the price
-  // universalTruckingPage.assertParMoiTxt();
-  // Click on Coordonnees button
-  await universalTruckingPage.clickBtnCoordonnées();
-});
+// test.beforeEach(async ({ page }) => {
+//   universalTruckingPage = new UniversalTruckingPage(page);
+//   // test.slow triples the default wait times
+//   test.slow();
+//   // await checkoutConfirmationPage.navigateToCheckoutConfirmationPage('Alaska');
+// });
+// test('Universal Trucking Group', async ({ page }) => {
+//   test.slow;
+//   // Go to https://www.shieldbenefits.com/bestmoneymoves/overview
+//   await universalTruckingPage.navigateTo1UniversalTruckingGroupPage();
+//   // Confirm button takes user to W3 URL
+//   await universalTruckingPage.assertW3Url();
+//   // Click on SignUp tab
+//   await universalTruckingPage.clickTabSigningUp();
+//   // Select a language
+//   await universalTruckingPage.selectlanguage('French');
+//   // Select a state
+//   await universalTruckingPage.selectStateUniversalTruckingPage('Texas');
+//   await universalTruckingPage.clickBtnESelect();
+//   // Select a plan
+//   await universalTruckingPage.clickBtnSelectPlan('Sélectionnez votre plan juridique');
+//   // Verify the Plan name
+//   await universalTruckingPage.assertSelectedPlanAndParMoiTxt();
+//   // Verify the price
+//   // universalTruckingPage.assertParMoiTxt();
+//   // Click on Coordonnees button
+//   await universalTruckingPage.clickBtnCoordonnées();
+// });
