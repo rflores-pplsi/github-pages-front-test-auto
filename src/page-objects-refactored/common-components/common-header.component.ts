7b8fe7fc9ad7e10563534e2b1ca5eae0c4ac7046
mattfee <mattfee@legalshieldcorp.com>
2023-03-24 11:47:26 -0600

    Qaops 311/language drop down (#169)
    * video plays tests passing
    
    * Language dropdown test cases

--- src/page-objects-refactored/common-components/common-header.component.ts
+++ src/page-objects-refactored/common-components/common-header.component.ts
@@ -7,11 +7,16 @@ export class CommonHeaderComponent {
   readonly locLargeLogo: Locator;
   readonly locPageLabel: Locator;
   readonly locHelpButton: Locator;
-  readonly locGlobeButton: Locator;
   readonly locHelpMenuContainer: Locator;
   readonly locSalesAndCustomerServicePhoneLink: Locator;
   readonly locMemberServicesEmailLink: Locator;
   readonly locAssociateSupportPhoneLink: Locator;
+  readonly locGlobeButton: Locator;
+  readonly locMarketLanguageMenuContainer: Locator;
+  readonly locUnitedStatesEnglishOption: Locator;
+  readonly locEstadosUnidosOption: Locator;
+  readonly locCanadaEnglishOption: Locator;
+  readonly locCanadaFrancaisOption: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -20,8 +25,7 @@ export class CommonHeaderComponent {
     this.locLargeLogo = this.page.locator('#lsdsLargeLogoId');
     this.locPageLabel = this.page.locator('#lsux-page-title');
     this.locHelpButton = this.page.locator('#lsdsHelpButtonId');
-    this.locGlobeButton = this.page.locator('#lsdsLanguageButtonId');
-    this.locHelpMenuContainer = this.page.locator('#lsdsLanguageDropdownId');
+    this.locHelpMenuContainer = this.page.locator('#lsdsHelpContentDefaultId');
     this.locSalesAndCustomerServicePhoneLink = this.page.locator(
       '//div[@id="lsdsHelpContentDefaultId"]/div/div[contains(.,"Sales and/or Customer Service")]/following-sibling::div//a[contains(.,"1-800-654-7757")]'
     );
@@ -31,5 +35,23 @@ export class CommonHeaderComponent {
     this.locAssociateSupportPhoneLink = this.page.locator(
       '//div[@id="lsdsHelpContentDefaultId"]/div/div[contains(.,"Associate Support")]/following-sibling::div//a[contains(.,"580-436-7424")]'
     );
+    this.locGlobeButton = this.page.locator('#lsdsLanguageButtonId');
+    this.locMarketLanguageMenuContainer = this.page.locator('#lsdsLanguageDropdownId');
+    this.locUnitedStatesEnglishOption = this.page.locator('//span[@id="lsdsLanguageDropdownId"]//a[contains(.,"United States - English")]');
+    this.locEstadosUnidosOption = this.page.locator('//span[@id="lsdsLanguageDropdownId"]//a[contains(.,"Estados Unidos - Español")]');
+    this.locCanadaEnglishOption = this.page.locator('//span[@id="lsdsLanguageDropdownId"]//a[contains(.,"Canada - English")]');
+    this.locCanadaFrancaisOption = this.page.locator('//span[@id="lsdsLanguageDropdownId"]//a[contains(.,"Canada - Français")]');
   }
+
+  /**
+   *
+   *
+   * @param {string} option
+   * @memberof CommonHeaderComponent
+   */
+  selectMarketLanguage = async (option: string): Promise<void> => {
+    await this.locGlobeButton.click();
+    const optionLocator = this.page.locator(`//span[@id="lsdsLanguageDropdownId"]//a[contains(.,"${option}")]`);
+    await optionLocator.click();
+  };
 }
--- src/tests/common-components/common-header.spec.ts
+++ src/tests/common-components/common-header.spec.ts
@@ -23,3 +23,74 @@ test('Verify Help Drop Down options', async ({ page }) => {
     await expect(commonHeaderComponent.locAssociateSupportPhoneLink).toBeVisible();
   });
 });
+
+test('Verify Language Drop Down options', async ({ page }) => {
+  console.log('Test Case: Verify Language Drop Down options');
+  await test.step(`Navigate to login service`, async () => {
+    await page.goto(UrlsUtils.legalshieldUrls.login.url);
+  });
+  await test.step(`Click on the Globe button in the Header of Login Service`, async () => {
+    await commonHeaderComponent.locGlobeButton.click();
+  });
+  await test.step(`Verify the Help Menu contents`, async () => {
+    await expect(commonHeaderComponent.locUnitedStatesEnglishOption).toBeVisible();
+    await expect(commonHeaderComponent.locEstadosUnidosOption).toBeVisible();
+    await expect(commonHeaderComponent.locCanadaEnglishOption).toBeVisible();
+    await expect(commonHeaderComponent.locCanadaFrancaisOption).toBeVisible();
+  });
+});
+
+test('Selecting United States - English option from Market-Language adds /?market=en-US query string to the URL', async ({ page }) => {
+  console.log('Test Case: Selecting United States - English option from Market-Language adds /?market=en-US query string to the URL');
+  await test.step(`Navigate to login service`, async () => {
+    await page.goto(UrlsUtils.legalshieldUrls.login.url);
+  });
+  await test.step(`Select different Market-Language Option`, async () => {
+    await commonHeaderComponent.selectMarketLanguage('Estados Unidos - Español');
+  });
+  await test.step(`Select United States - English Option`, async () => {
+    await commonHeaderComponent.selectMarketLanguage('United States - English');
+  });
+  await test.step(`Verify the URL contains the /?market=en-US`, async () => {
+    await expect(page).toHaveURL(new RegExp('/?market=en-US'));
+  });
+});
+
+test('Selecting Estados Unidos - Español option from Market-Language adds /?market=es-US query string to the URL', async ({ page }) => {
+  console.log('Test Case: Selecting Estados Unidos - Español option from Market-Language adds /?market=es-US query string to the URL');
+  await test.step(`Navigate to login service`, async () => {
+    await page.goto(UrlsUtils.legalshieldUrls.login.url);
+  });
+  await test.step(`Select Estados Unidos - Español Option`, async () => {
+    await commonHeaderComponent.selectMarketLanguage('Estados Unidos - Español');
+  });
+  await test.step(`Verify the URL contains the /?market=es-US`, async () => {
+    await expect(page).toHaveURL(new RegExp('/?market=es-US'));
+  });
+});
+
+test('Selecting Canada - English - Español option from Market-Language adds /?market=en-CA query string to the URL', async ({ page }) => {
+  console.log('Test Case: Selecting Canada - English - Español option from Market-Language adds /?market=en-CA query string to the URL');
+  await test.step(`Navigate to login service`, async () => {
+    await page.goto(UrlsUtils.legalshieldUrls.login.url);
+  });
+  await test.step(`Select Canada - English Market-Language Option`, async () => {
+    await commonHeaderComponent.selectMarketLanguage('Canada - English');
+  });
+  await test.step(`Verify the URL contains the /?market=en-CA`, async () => {
+    await expect(page).toHaveURL(new RegExp('/?market=en-CA'));
+  });
+});
+
+test('Selecting Canada - Français option from Market-Language adds /?market=fr-CA query string to the URL', async ({ page }) => {
+  console.log('Test Case: Selecting Canada - Français option from Market-Language adds /?market=fr-CA query string to the URL');
+  await test.step(`Navigate to login service`, async () => {
+    await page.goto(UrlsUtils.legalshieldUrls.login.url);
+  });
+  await test.step(`Select Canada - Français Option`, async () => {
+    await commonHeaderComponent.selectMarketLanguage('Canada - Français');
+  });
+  await test.step(`Verify the URL contains the /?market=fr-CA`, async () => {
+    await expect(page).toHaveURL(new RegExp('/?market=fr-CA'));
+  });
+});
