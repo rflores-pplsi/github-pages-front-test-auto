62c9bf926123cc20850ff1750d46360d8eeb51b7
rflores-pplsi <109102578+rflores-pplsi@users.noreply.github.com>
2024-01-04 13:03:44 -0500

    QAOPS-515 adding test for app store and google play links (#235)

--- src/page-objects/marketing-sites/legalshield/legalshield-service.ts
+++ src/page-objects/marketing-sites/legalshield/legalshield-service.ts
@@ -57,7 +57,7 @@ export class LegalshieldService {
       'body .lsux-link[href]:not([target="_blank"]):not([href*="javascript:void(0)"]):not([href*="#"]), body .lsux-button--primary[href]:not([target="_blank"]):not([href*="javascript:void(0)"]):not([href*="#"])'
     );
     this.locLinksThatNavigateToNewTab = this.page.locator(
-      'body .lsux-link[href]:is([target="_blank"]), body .lsux-button--primary[href]:is([target="_blank"])'
+      'body .lsux-link[href]:is([target="_blank"]), body .lsux-button--primary[href]:is([target="_blank"]), body .lsux-download-app a[href]:is([target="_blank"])'
     );
 
     this.locContainersWithAddToCartLinks = this.page.locator(
@@ -259,7 +259,6 @@ export class LegalshieldService {
       });
     }
   };
-
   clickNavigationLocatorsAndVerifyNewTabURLWithoutError = async (locator: Locator): Promise<void> => {
     const locators = await locator.all();
     let newTab: Page;
