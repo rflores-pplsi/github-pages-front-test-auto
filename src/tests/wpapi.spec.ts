1eaf8eabf21dd1b98730552f580a841ebe4b60dd
rflores-pplsi <109102578+rflores-pplsi@users.noreply.github.com>
2024-03-07 13:43:43 -0500

    QAOPS-538 adding initial tests (#270)
    * QAOPS-538 adding initial tests
    
    * QAOPS-538 adding first expect, working
    
    * QAOPS-538 finalizing fetch method to return desired output
    
    * QAOPS-538 adding jdocs correctly
    
    * QAOPS-538 remove tests from file,for future use only

--- src/tests/wpapi.spec.ts
+++ src/tests/wpapi.spec.ts
@@ -8,9 +8,6 @@ interface Page {
     rendered: string;
   };
   link: string;
-  content: {
-    rendered: string;
-  };
   // Add other fields as needed based on the actual post structure
 }
 
@@ -18,54 +15,53 @@ interface Page {
  * Fetches all the pages that are assigned to the DS2 templating structure.
  * @param {number} pageNo
  * @param {number} perPage
- * @return {Array} allPages
+ * @return {Promise} Page object
  */
-async function fetchPages(pageNo = 1, perPage = 10): Promise<Array<Page[]>> {
+async function fetchPages(pageNo: number, perPage: number): Promise<Page[]> {
   const apiUrl = `${UrlsUtils.legalshieldService.baseUrl}/wp-json/wp/v2/pages`;
   const pageTemplate = 'lsus_pages.php';
   const status = 'publish';
   const fetchUrl = `${apiUrl}/?per_page=${perPage}&template=${pageTemplate}&page=${pageNo}&status=${status}`;
-  const allPages: Array<Page[]> = [];
-
+  const allPages: Page[] = [];
   try {
     const response = await fetch(fetchUrl);
     const pages: Page[] = await response.json();
     if (pages.length === 0) {
       return allPages;
     }
-    allPages.push(pages);
+    pages.forEach((page) => {
+      allPages.push(page);
+    });
 
-    return await fetchPages(pageNo + 1, perPage);
+    return allPages; // testing the return value of single fetchPages method
+    // return await fetchPages(pageNo + 1, perPage);
   } catch (error: unknown) {
     console.error('error fetching posts: ', error instanceof Error ? error.message : error);
     throw error;
   }
 }
-/**
- * Collects all the pages
- *
- */
-fetchPages()
-  .then((allPages) => {
-    console.log(allPages);
-  })
-  .catch((error) => {
-    console.error('error fetching pages: ', error);
-  });
 // @TODO add test for each set of ten simulataneously ( 10 workers )
-// test.describe('Testing out the collection of pages from WP Rest API', () => {
-//   test.setTimeout(800000);
-//   test('testing fetchPages function', async () => {
-//     // expect(await fetchPages()).toBeDefined();
-//     // console.log(`count: ${counter} pages`);
-//     expect(
-//       await fetchPages()
-//         .then((allPages) => {
-//           console.log(allPages);
-//         })
-//         .catch((error) => {
-//           console.error('error fetching pages: ', error);
-//         })
-//     ).toBeTruthy();
+// test.describe.configure({ mode: 'parallel', timeout: 50_000, workers: 10 }, ({ page }) => {
+//   test('test 1', async ({ page }) => {
+//     // Test implementation
 //   });
+
+//   test('test 2', async ({ page }) => {
+//     // Test implementation
+//   });
+// });
+// configure parallel mode for all subsequent tests
+// test.describe.configure({ mode: 'parallel', timeout: 50_000 });
+// test 1
+// test('test 1: run all tests at once in parallel mode', async ({ page }) => {
+//   const allPages = await fetchPages(1, 5);
+//   console.log(allPages);
+//   // Add your tests here
+//   const links = allPages.map((newPage) => newPage.link);
+//   for (const link of links) {
+//     await page.goto(link);
+//     console.log('pageURL: ', page.url());
+//     console.log('link: ', link);
+//     expect(page.url()).toBe(link);
+//   }
 // });
