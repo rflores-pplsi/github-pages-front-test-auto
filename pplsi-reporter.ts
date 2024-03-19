5a38d0c531d49ec66da9c1db4e4291e7e6667d80
rflores-pplsi <109102578+rflores-pplsi@users.noreply.github.com>
2024-03-19 14:21:45 -0400

    QAOPS-553 reporting each individual test gives us better results (#276)

--- pplsi-reporter.ts
+++ pplsi-reporter.ts
@@ -1,14 +1,14 @@
 import type { Reporter, FullConfig, Suite, TestCase, TestResult, FullResult } from '@playwright/test/reporter';
 import * as dotenv from 'dotenv';
+
+interface reportData {
+  duration: number;
+  status: string;
+  title: string;
+}
 class MyReporter implements Reporter {
   private apiNewRelicUrl = 'https://insights-collector.newrelic.com/v1/accounts/124794/events';
-  private reportData = [
-    {
-      duration: 0,
-      status: 'default',
-      title: 'Test Result Title',
-    },
-  ];
+  private reportData: reportData[] = []; // Add reportData property
 
   onBegin(config: FullConfig, suite: Suite) {
     // console.log(`onBegin:: Starting the run with ${suite.allTests().length} tests`);
@@ -34,11 +34,6 @@ class MyReporter implements Reporter {
       status: result.status,
       title: test.title,
     });
-    // console.log(`onTestEnd:: Finished test ${test.title}: ${result.status}`);
-  }
-
-  onEnd(result: FullResult) {
-    const allResults = JSON.stringify(this.reportData);
     // Send to New Relic Ingestion API
     const apiKey = process.env.API_NEW_RELIC_INGEST_KEY || '';
     const headersInfo = {
@@ -47,18 +42,45 @@ class MyReporter implements Reporter {
     };
     const body = JSON.stringify([
       {
+        duration: result.duration,
         eventType: 'PlaywrightReporter',
-        result: `reportData: ${allResults}`,
+        status: result.status,
+        title: test.title,
       },
     ]);
 
     fetch(this.apiNewRelicUrl, { body: body, headers: headersInfo, method: 'POST' })
-      .then((response) => response.json())
-      // .then((data) => console.log('data sent to NewRelic:' + allResults))
+      //.then((response) => response.json())
+      .then((response) => console.log(`Test: ${test.title} send successfully? ` + response.ok))
       .catch((error) => {
         console.error('Error:', error);
       });
     // console.log(`onEnd:: Finished the run: status: ${status}, startTime: ${startTime}, duration: ${duration}, reportData: ${allResults}`);
+    // console.log(`onTestEnd:: Finished test ${test.title}: ${result.status}`);
+  }
+
+  onEnd(result: FullResult) {
+    // const allResults = JSON.stringify(this.reportData);
+    // const allResultsString = allResults.toString();
+    // // Send to New Relic Ingestion API
+    // const apiKey = process.env.API_NEW_RELIC_INGEST_KEY || '';
+    // const headersInfo = {
+    //   'Content-Type': 'application/json',
+    //   'X-Insert-Key': apiKey,
+    // };
+    // const body = JSON.stringify([
+    //   {
+    //     eventType: 'PlaywrightReporter',
+    //     result: allResultsString,
+    //   },
+    // ]);
+    // fetch(this.apiNewRelicUrl, { body: body, headers: headersInfo, method: 'POST' })
+    //   //.then((response) => response.json())
+    //   .then((data) => console.log('data sent to NewRelic success? ' + allResultsString))
+    //   .catch((error) => {
+    //     console.error('Error:', error);
+    //   });
+    // // console.log(`onEnd:: Finished the run: status: ${status}, startTime: ${startTime}, duration: ${duration}, reportData: ${allResults}`);
   }
 }
 export default MyReporter;
